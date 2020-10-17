f365869dabf553ac751461524f6489b536c3e1d7
Bryan Ezequiel Valdez Calderón <bryan501th@gmail.com>
2020-10-16 22:04:12 -0500

    Se me olvidó ejecutar, éste commit debería ser el anterior

--- src/PruebaFilas.java
+++ src/PruebaFilas.java
@@ -1,8 +1,59 @@
+interface RegistroImpresiones{
+	public boolean filaLlena();
+	public void insertar();
+	public boolean filaVacia();
+	public Impresion sacar();
+	public void mostrarFrente();
+	public void totalImpresas();
+	public void totalBytes();
+}
+
+class Impresion{
+	private int id;
+	private int tamaño;
+	private int cntHojas;
+	
+	public Impresion() {
+	}
+	public Impresion(int id, int tamaño, int cntHojas) {
+		this.id = id;
+		this.tamaño = tamaño;
+		this.cntHojas = cntHojas;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getTamaño() {
+		return tamaño;
+	}
+	public void setTamaño(int tamaño) {
+		this.tamaño = tamaño;
+	}
+	public int getCntHojas() {
+		return cntHojas;
+	}
+	public void setCntHojas(int cntHojas) {
+		this.cntHojas = cntHojas;
+	}
+	
+	@Override
+	public String toString() {
+		return "Impresion [id=" + id + ", tamaño=" + tamaño + ", cntHojas=" + cntHojas + "]";
+	}	
+
+}
+
+
 
 public class PruebaFilas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
