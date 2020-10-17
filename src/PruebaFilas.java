2c8f272fff2070bb31bc37e307b893ebc678da3c
Bryan Ezequiel Valdez Calderón <bryan501th@gmail.com>
2020-10-16 22:13:10 -0500

    Se creo la clase ImplementacionFilaEstatica

--- src/PruebaFilas.java
+++ src/PruebaFilas.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 interface RegistroImpresiones{
 	public boolean filaLlena();
 	public void insertar();
@@ -47,6 +49,57 @@ class Impresion{
 
 }
 
+class ImplementacionFilaEstatica implements RegistroImpresiones{
+	private Impresion registro[];
+	private int frente;
+
+	public ImplementacionFilaEstatica() {
+	}
+	public ImplementacionFilaEstatica(int tamaño) {
+		this.registro = new Impresion[tamaño];
+		this.frente = -1;
+	}
+	
+	public Impresion[] getRegistro() {
+		return registro;
+	}
+	public void setRegistro(Impresion[] registro) {
+		this.registro = registro;
+	}
+	public int getFrente() {
+		return frente;
+	}
+	public void setFrente(int frente) {
+		this.frente = frente;
+	}
+	public boolean filaLlena() {
+		
+		return false;
+	}
+	
+	public void insertar() {
+		
+	}
+	public boolean filaVacia() {
+		return false;
+	}
+	public Impresion sacar() {
+		return null;
+	}
+	public void mostrarFrente() {
+	}
+	public void totalImpresas() {
+	}
+	public void totalBytes() {
+	}
+	
+	@Override
+	public String toString() {
+		return "ImplementacionFilaEstatica [registro=" + Arrays.toString(registro) + ", frente=" + frente + "]";
+	}
+	
+	
+}
 
 
 public class PruebaFilas {
